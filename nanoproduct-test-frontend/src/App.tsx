99519c6513c9622384d92890b4e56aa67346649e
Stas Pikuza <s.pikuza@minefintech.com>
2023-05-03 16:14:03 +0300

    init App

---
+++ nanoproduct-test-frontend/src/App.tsx
@@ -0,0 +1,13 @@
+import React, {LegacyRef, RefObject, useEffect, useRef} from 'react';
+import './App.css';
+
+function App() {
+
+    return (
+        <div className="App">
+            <h1>Hello world</h1>
+        </div>
+    );
+}
+
+export default App;
